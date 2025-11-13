2693fe9c90ebef82e10f5dd2760676907706288a
Erwin <tapiaerwim@gmail.com>
2025-11-13 09:26:15 -0600

    cambios

---
+++ WEB2/Presentacion/Administrador/menu.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>Panel de Adminitracion</title>
+  <link rel="stylesheet" href="../Diseño/Administrador/panel.css">
+</head>
+<body>
+  <header>
+    <h1>Adminitracion Escolar</h1>
+    <nav>
+      <a href="gestion_alumnos.php">Gestion Alumnos</a>
+      <a href="listaMaterias.php">Gestion Maestros</a>
+      <a href="perfil.php">Gestion Materias</a>
+      <a href="../index.html"> Cerrar sesión</a>
+    </nav>
+  </header>
+
+  <main>
+  <h2>Inicio</h2>
+  <p>Hola, bienvenido al sistema de control escolar.</p>
+
+  <section class="dashboard">
+    <div class="card">
+      <h3>Alumnos registrados</h3>
+      <p>120</p>
+    </div>
+    <div class="card">
+      <h3>Maestros</h3>
+      <p>8</p>
+    </div>
+    <div class="card">
+      <h3>Materias activas</h3>
+      <p>15</p>
+    </div>
+    <div class="card">
+      <h3>Inscripciones</h3>
+      <p>180</p>
+    </div>
+  </section>
+</main>
+
+  <footer>
+  <div class="footer-izquierda">
+    <img src="../Imagenes/logo_tec.png" alt="Logo Instituto" class="logo-footer">
+    <div class="footer-info">
+      <p><strong>Instituto Tecnológico Superior de San Nicolás de Hidalgo</strong></p>
+      <p>Teléfono: 445 192 8872</p>
+    </div>
+  </div>
+
+  <div class="footer-derecha">
+    <p>Sistema de Administración Escolar © 2025</p>
+    <p>Desarrollado por el área de informática</p>
+  </div>
+</footer>
+
+
+</body>
+</html>
