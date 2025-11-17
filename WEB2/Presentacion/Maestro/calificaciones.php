0479bd37341298b8bf2984eec185131007ad2820
Juan <s22120254@alumnos.itsur.edu.mx>
2025-11-16 19:41:03 -0600

    se agrego la estructura para todo lo relacionado con Maestro

---
+++ WEB2/Presentacion/Maestro/calificaciones.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+
+// Validar sesión
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Calificaciones</title>
+    <link rel="stylesheet" href="../Diseño/Maestro/panel.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
+<body>
+
+</body>
+</html>
---
+++ WEB2/Presentacion/Maestro/menu.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+
+// Validar sesión
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>Panel del Maestro</title>
+  <link rel="stylesheet" href="../Diseño/Maestro/panel.css">
+</head>
+<body>
+  
+</body>
+</html>
