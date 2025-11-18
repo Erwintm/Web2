aa6eb3219b6ddf1b9cdab0c72e21de0e05782a25
Erwin <tapiaerwim@gmail.com>
2025-11-17 18:26:13 -0600

    editar ya jala,login sha2

--- WEB2/Presentacion/Administrador/editar_alumno.php
+++ WEB2/Presentacion/Administrador/editar_alumno.php
@@ -124,7 +124,24 @@ if (!$alumno) {
 
     </form>
 
+    
+
 </div>
+
+<footer>
+  <div class="footer-izquierda">
+    <img src="../Imagenes/logo_tec.png" alt="Logo Instituto" class="logo-footer">
+    <div class="footer-info">
+      <p><strong>Instituto Tecnológico Superior de San Nicolás de Hidalgo</strong></p>
+      <p><strong>Correo: instecsanico@gmail.com</strong></p>
+    </div>
+  </div>
+
+  <div class="footer-derecha">
+    <p><strong>Sistema de Administración Escolar 2025</strong></p>
+    <p><strong>Desarrollado por el área de informática</strong></p>
+  </div>
+</footer>
 
 </body>
 </html>
