22fcee328f0878161bc5eda90bab9e017ef756d6
Erwin <tapiaerwim@gmail.com>
2025-11-18 13:38:57 -0600

    eliminar maestros xd

--- WEB2/Negocio/eliminar_alumno.php
+++ WEB2/Negocio/eliminar_alumno.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../Datos/conexion.php'; // aquí se crea $pdo
+require_once '../Datos/conexion.php'; 
 
 if (!isset($_POST['matricula'])) {
     echo "Error: No se recibió la matrícula.";
@@ -9,17 +9,25 @@ if (!isset($_POST['matricula'])) {
 $matricula = $_POST['matricula'];
 
 try {
-    $sql = "DELETE FROM alumnos WHERE matricula = :matricula";
-    $stmt = $pdo->prepare($sql);
-    $stmt->bindParam(':matricula', $matricula);
 
-    if ($stmt->execute()) {
-        echo "Alumno eliminado correctamente.";
-    } else {
-        echo "No se pudo eliminar el alumno.";
+    $sql = "SELECT id_usuario FROM alumnos WHERE matricula = :matricula";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([':matricula' => $matricula]);
+
+    $id_usuario = $stmt->fetchColumn();
+
+    if (!$id_usuario) {
+        echo "Alumno no encontrado.";
+        exit;
     }
+
+   
+    $sqlDel = "DELETE FROM usuarios WHERE id_usuario = :id";
+    $stmtDel = $pdo->prepare($sqlDel);
+    $stmtDel->execute([':id' => $id_usuario]);
+
+    echo "Alumno eliminado correctamente.";
 
 } catch (Exception $e) {
     echo "Error: " . $e->getMessage();
 }
-?>
