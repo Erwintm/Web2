c0904de1cb6c10292c862284648f8094a650b33b
Juan <s22120254@alumnos.itsur.edu.mx>
2025-11-23 15:56:33 -0600

    Cambiar el cerrar sesion

--- WEB2/Presentacion/Maestro/menu.php
+++ WEB2/Presentacion/Maestro/menu.php
@@ -30,7 +30,7 @@ require_once '../../Negocio/obtener_resumen_maestro.php';
       <a href="gestion_materias.php">Mis Materias</a>
       <a href="calificaciones.php">Calificaciones</a>
       <a href="perfil.php">Mi Perfil</a>
-      <a href="../../Presentacion/index.html">Cerrar sesión</a>
+      <a href="../../Negocio/logout.php">Cerrar sesión</a>
     </nav>
   </header>
 
